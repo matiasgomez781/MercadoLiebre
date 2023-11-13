a7f31d2ad82497753479f535e25bfa873143f5d8
matiasgomez781 <darksho781@gmail.com>
2023-11-13 04:37:34 -0300

    Se creo ruta para login.html en app.js

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
+/*Ruta Login*/
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
 /*Post para formulario - Regresa al home*/
 app.post("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
